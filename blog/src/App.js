20633c185b616612891967bf06fae800f023a20d
ohwooje <dkwooje@naver.com>
2024-11-21 17:05:29 +0900

    var let const 차이 6. const

--- blog/src/App.js
+++ blog/src/App.js
@@ -1,13 +1,30 @@
 
-console.log(num); //undefined이 선언된다.(호이스팅(선언문 이전에 접근))
+//const a = 'hi'; //const를 let으로 바꿔보자
+//a= 'hello'; //const는 재할당이 불가능하다.
 
-var num = 10;
-//let num = 10; //let은 호이스팅 하지만 var와 같이 초기화 하지 않는다.(일시적 사각지대 TDZ)
- 
-console.log(num); //10이 선언된다.
 
+const b ={
+  x: 1,
+  y: 2,
+}
+
+//b={z:3} //객체에 추가하는것은 불가
+b.x = 145;  //객체안의 속성을 변경은 가능하다.
+
+console.log(b);
+
+const c = Object.freeze({ //만약 객체안의 속성의 변경을 막고 싶다면  Object.freeze를 사용해 보자.
+  x: 1,
+  y: 2,
+});
+
+b.x = 145; 
+
+console.log(c);
 
 function App() {
 
 }
 export default App;
+//const : block Scope, 중복 선언 불가, 선언문 이전 접근 불가 (let과 같다.)
+//단 const는 let과 달리 상수를 선언한다.(한번 값을 할당하면 재할당 불가)
