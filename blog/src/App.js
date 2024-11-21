24d955ac97ab0658511f1c23dfbb10a5e088ffef
ohwooje <dkwooje@naver.com>
2024-11-21 17:30:54 +0900

    this 에 대해 bind

--- blog/src/App.js
+++ blog/src/App.js
@@ -1,20 +1,9 @@
 
 function main(){
-  console.log(this); //함수가 object 밖에서 정의 되어도 this는 object를 가르킨다.
+  console.log(this); 
 }
-
-const object = {
-  name: '코딩',
-  //main,
-  smallObject:{
-    name: '작은 코딩',
-    main,
-  }
-  };
-
-//object.main(); //메인함수를 호출한 객체가 object이기 때문이다.
-object.smallObject.main();
-//main값을 직접 호출한 객체는 smallObject이기 때문에 this는 smallObject가 된다.
+const mainBind = main.bind({name: 'hi'}); //bind 새로운 함수를 반환해준다.
+mainBind();                           
 
 
 function App() {
