389a7e04f07e1976d42376c5eff73d981b544c70
ohwooje <dkwooje@naver.com>
2024-11-21 16:47:10 +0900

    var let const 차이 4. var 와 let window

--- blog/src/App.js
+++ blog/src/App.js
@@ -1,14 +1,17 @@
 
 
+var aVar = 'varHello';
+let aLet = 'letHello';
+
+console.log(window);
+
+
+//위의 두 변수는 함수로 감싸져 있지 않는 전역변수이다
+
+
+
 function App() {
 
-  for(let i = 0; i<2; i++){  //var i 와 구분해 보자
-    console.log(i); 
-  }
- // console.log(i); // var 변수에만 유효하다. 그리고 추가적으로 계산 된다. 즉 for문 이상 출력된다.
 }
 export default App;
 
-//Var와 let의 비교 (1.스코프 2.중복 선언 3.호이스팅)
-//1.스코프(범위): 변수가 접근할 수 있는 범위
-//스코프의 종류: 1.함수범위(var) 2.블록범위(let) 3.전역범위
