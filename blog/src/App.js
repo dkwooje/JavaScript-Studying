6ed90dca5963e794ebf82ff93770df0645d5f7e9
ohwooje <dkwooje@naver.com>
2024-11-21 16:51:01 +0900

    var let const 차이 5. var 와 let 중복선언

--- blog/src/App.js
+++ blog/src/App.js
@@ -1,17 +1,16 @@
 
+var x = "안녕하세요"; //let x
 
-var aVar = 'varHello';
-let aLet = 'letHello';
+//......
 
-console.log(window);
+var x = "hi"; //let x
 
-
-//위의 두 변수는 함수로 감싸져 있지 않는 전역변수이다
-
-
+console.log(x);
 
 function App() {
 
 }
 export default App;
-
+//같은이름의 변수를 중복 선언하면 보통 코드는 에러가 발생한다.
+//하지만 var는 에러가 발생하지 않는다. 중복선언이 가능하다. 이 점이 매우 안좋다.
+//let 변수는 중복선언이 되지 않는다.
