b4a3949ec562e56d5498aaf6d4571902c2623b7d
ohwooje <dkwooje@naver.com>
2024-11-21 17:22:17 +0900

    this 에 대해2

--- blog/src/App.js
+++ blog/src/App.js
@@ -1,15 +1,20 @@
 
-const object = {
-  name: '코딩',
-  main: function(){
-    console.log(this); //함수 안의 this는 함수를 호출하는 객체가 된다.
-  }
+function main(){
+  console.log(this); //함수가 object 밖에서 정의 되어도 this는 object를 가르킨다.
 }
 
-object.main(); //main은 obtect를 호출한다. 그러므로 this는 object가 된다.
+const object = {
+  name: '코딩',
+  //main,
+  smallObject:{
+    name: '작은 코딩',
+    main,
+  }
+  };
 
-
-
+//object.main(); //메인함수를 호출한 객체가 object이기 때문이다.
+object.smallObject.main();
+//main값을 직접 호출한 객체는 smallObject이기 때문에 this는 smallObject가 된다.
 
 
 function App() {
