1570e2525f965e04b7474bcb6440d4135a74623a
ohwooje <dkwooje@naver.com>
2024-11-21 16:42:01 +0900

    var let const 차이 4. var 와 let for문 구분

--- blog/src/App.js
+++ blog/src/App.js
@@ -2,14 +2,11 @@
 
 function App() {
 
-  let x = "hello"; //두 변수 x를 var로 바꿔 비교해보자
-  if(true){
-    let x ='hi';  //var로 바꾸면 아래의 변수가 위의 변수를 덮어 씌운다.
-    console.log(x); 
+  for(let i = 0; i<2; i++){  //var i 와 구분해 보자
+    console.log(i); 
   }
-  console.log(x);
+ // console.log(i); // var 변수에만 유효하다. 그리고 추가적으로 계산 된다. 즉 for문 이상 출력된다.
 }
- 
 export default App;
 
 //Var와 let의 비교 (1.스코프 2.중복 선언 3.호이스팅)
