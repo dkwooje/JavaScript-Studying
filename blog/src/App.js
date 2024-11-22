6da57c1f668c690b81bcc613803ff64a0e083613
ohwooje <dkwooje@naver.com>
2024-11-22 18:10:51 +0900

    Promise 대기

--- blog/src/App.js
+++ blog/src/App.js
@@ -2,38 +2,21 @@
 
 function App() {
 
-  function login(username, callback){
-    setTimeout(()=>{
-      callback(username);
-    }, 1000);
-  }
+const promise = new Promise((resolve,reject)=>{ //Promise라는 상자 안에 excicutor라는 함수를 전달 받는다. 
+                                                    
+setTimeout(()=>{                              
+      const data = {name: '철수'};
+      console.log('네트워크 요청 성공');
+  }, 1000);                   
+});
+          
+  console.log(promise);
 
 
-  function addToCart(porduct, callback){
-    setTimeout(()=>{
-      callback(porduct);
-    }, 1000);
-  }
 
-  function makePayment(cardNumber, porduct, callback){
-    setTimeout(()=>{
-      callback(cardNumber, porduct);
-    }, 1000);
-  }
-
-
-  login('별코딩', (username) =>{
-    console.log(`${username}님 안녕하세요`);
-  addToCart("감자", (porduct) => {
-    console.log(`${porduct}를 장바구니에 넣었습니다.`);
-  makePayment("00000000000000","감자", (cardNumber,product) => {
-    console.log(`${cardNumber.slice(0,6)}로 ${product}를 구매했습니다.`); //slice(첫 6자리만 출력)
-      });
-    });
-  });
 }
-
-
 export default App;
-//비동기 콜백
-//비동기 방식의 함수처리후 진행해야하는 함수가 있을 때 사용한다. 비동기-> 비동기 콜백->비동기 후 진행 해야하는 함수
+//pormise : 비동기 처리에 사용되는 자바스크립트 객체 
+//비동기 작업이 맞이할 성공 혹은 실패를 나타냄
+//promise 3가지 상태 : 대기(pending), 성공(fulfilled), 실패(rejected))
+//대기 :undefined , 성공 :result , 실패:error 를 구현
