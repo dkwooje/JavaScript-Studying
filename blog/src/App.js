02fee03a19ec7f9096bc21c8e6c3d348bb3ea0d1
ohwooje <dkwooje@naver.com>
2024-11-22 18:11:57 +0900

    Promise 성공

--- blog/src/App.js
+++ blog/src/App.js
@@ -3,20 +3,22 @@
 function App() {
 
 const promise = new Promise((resolve,reject)=>{ //Promise라는 상자 안에 excicutor라는 함수를 전달 받는다. 
-                                                    
-setTimeout(()=>{                              
+                             //성공 resolve 호출, 실패 reject 호출
+    setTimeout(()=>{  
       const data = {name: '철수'};
       console.log('네트워크 요청 성공');
+      resolve(data)
   }, 1000);                   
 });
-          
-  console.log(promise);
 
+setTimeout(()=>{                            
+  console.log(promise);
+}, 2000);                   
 
 
 }
 export default App;
 //pormise : 비동기 처리에 사용되는 자바스크립트 객체 
 //비동기 작업이 맞이할 성공 혹은 실패를 나타냄
-//promise 3가지 상태 : 대기(pending), 성공(fulfilled), 실패(rejected))
+//promise 3가지 상태 : 대기(pending), 성공(fulfilled), 실패(rejected)
 //대기 :undefined , 성공 :result , 실패:error 를 구현
