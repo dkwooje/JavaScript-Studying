d696ed15f16928895af4e915b49cc996748fe7a0
ohwooje <dkwooje@naver.com>
2024-11-22 16:58:45 +0900

    콜백 함수의 응용2

--- blog/src/App.js
+++ blog/src/App.js
@@ -2,21 +2,12 @@
 
 function App() {
 
-   function greetToUser(greet){
-    const name = '네르';
-    //... 데이터 로딩//콜백 함수는 비동기 함수에 유용하다.
-    greet(name);
-   }
+  setTimeout(function() {
+    console.log("hi")
+  }, 1000); 
 
-   function greetInKorean(name){
-    console.log(name + "님, 안녕하세요");
-   }
+// 대표적인 콜백함수인 setTimeout이 있다.
 
-   function greetInEnglish(name){
-    console.log("HI, ms."+ name);
-   }
-             
-   greetToUser(greetInKorean);
-   greetToUser(greetInEnglish);
+
 }
 export default App;
