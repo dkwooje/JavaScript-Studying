930eae43940c16eae7087e4497876a5843983a02
ohwooje <dkwooje@naver.com>
2024-11-22 15:59:12 +0900

    화살표 함수의 여러 문법

--- blog/src/App.js
+++ blog/src/App.js
@@ -9,12 +9,19 @@ const main = function(){ //익명함수
 
   main() 
 
-  const main2 = () => { //화살표함수
+const main2 = () => { //화살표함수
     console.log("hi")
   }
   
     main2() 
 
+const main3 = () => console.log("hi")  //코드가 간결하면 중괄호와 return 생략 가능
+    
+      main3() 
+
+const main4 = () => ({saying: "hi"})      
+
+      console.log(main4())
 }
 export default App;
 //화살표 함수
