089005b478f399bb3efc54c02b2c0646e5a83bf6
ohwooje <dkwooje@naver.com>
2024-11-22 17:40:36 +0900

    비동기 콜백 응용 3

--- blog/src/App.js
+++ blog/src/App.js
@@ -12,26 +12,24 @@ function App() {
   function addToCart(porduct, callback){
     setTimeout(()=>{
       callback(porduct);
-    }, 2000);
+    }, 1000);
   }
 
   function makePayment(cardNumber, porduct, callback){
     setTimeout(()=>{
       callback(cardNumber, porduct);
-    }, 3000);
+    }, 1000);
   }
 
 
   login('별코딩', (username) =>{
     console.log(`${username}님 안녕하세요`);
-  });
-
   addToCart("감자", (porduct) => {
     console.log(`${porduct}를 장바구니에 넣었습니다.`);
-  });
-
   makePayment("00000000000000","감자", (cardNumber,product) => {
     console.log(`${cardNumber.slice(0,6)}로 ${product}를 구매했습니다.`); //slice(첫 6자리만 출력)
+      });
+    });
   });
 }
 
