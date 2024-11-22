c88d443bbfe710cb526ab169812c01fc967ea6ae
ohwooje <dkwooje@naver.com>
2024-11-22 16:33:21 +0900

    화살표 함수와  this

--- blog/src/App.js
+++ blog/src/App.js
@@ -1,22 +1,34 @@
 
 
 function App() {
-
-  const object1 ={
+/*
+  const object1 = {
     name: '객체1',
-    main: function () {
+    mainArrow: () => {
       console.log(this.name + '입니다.');
     }
   };
+  
+  object1.mainArrow();   //화살표 함수는 자신만의 this를 가지지 않는다.
+*/
+const object1 = {
+  name: '객체1',
+  mainArrow: function () {         // 일반 함수로 변경
+    console.log(this.name + '입니다.');
+  }
+};
 
-  object1.main(); //객체 1의 main을 호출한다. console의 this는 main을 호출한 객체 1를 가르킨다.
+object1.mainArrow();
 
-  const object2 ={
-    name: '객체2',
-    main: object1.main 
-  };
+const object2 = {
+  name: '객체2',
+  mainArrow: () => {
+    console.log(object2.name + '입니다.'); // object1을 직접 참조
+  }
+};
 
-  object2.main(); //객체 2의 main을 호출하고, 객체2의 main은 객체 1의 메인을 가리키지만 console의 this는 객체 2를 가르킨다.
+object2.mainArrow();
+
 
 }
 export default App;
