94d8ac55f7597f6e077c1108c272ff94e47b7522
ohwooje <dkwooje@naver.com>
2024-11-22 17:39:25 +0900

    비동기 콜백 응용 2

--- blog/src/App.js
+++ blog/src/App.js
@@ -2,20 +2,40 @@
 
 function App() {
 
-function getData(callBack){
-  setTimeout(()=>{
-    console.log('서버에서 데이터를 받았습니다.');
-    callBack({name: '컴퓨터 시작'});
-  }, 2000);
+  function login(username, callback){
+    setTimeout(()=>{
+      callback(username);
+    }, 1000);
+  }
+
+
+  function addToCart(porduct, callback){
+    setTimeout(()=>{
+      callback(porduct);
+    }, 2000);
+  }
+
+  function makePayment(cardNumber, porduct, callback){
+    setTimeout(()=>{
+      callback(cardNumber, porduct);
+    }, 3000);
+  }
+
+
+  login('별코딩', (username) =>{
+    console.log(`${username}님 안녕하세요`);
+  });
+
+  addToCart("감자", (porduct) => {
+    console.log(`${porduct}를 장바구니에 넣었습니다.`);
+  });
+
+  makePayment("00000000000000","감자", (cardNumber,product) => {
+    console.log(`${cardNumber.slice(0,6)}로 ${product}를 구매했습니다.`); //slice(첫 6자리만 출력)
+  });
 }
 
-getData((data)=>{
-  console.log(data.name);  
-});
 
-
-
-}
 export default App;
 //비동기 콜백
 //비동기 방식의 함수처리후 진행해야하는 함수가 있을 때 사용한다. 비동기-> 비동기 콜백->비동기 후 진행 해야하는 함수
