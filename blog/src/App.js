ed5d7e356f22697a9bbdaa624fcd6e6397b022de
ohwooje <dkwooje@naver.com>
2024-11-22 18:27:21 +0900

    Promise 비동기 함수 finally

--- blog/src/App.js
+++ blog/src/App.js
@@ -6,7 +6,7 @@ function App() {
   function getData(){
     const promise = new Promise((resolve,reject)=>{ 
       setTimeout(()=>{  
-        const data = null;
+        const data = {name: '철수'};
         if (data){
           console.log('네트워크 요청 성공');
           resolve(data)
@@ -23,6 +23,8 @@ function App() {
   console.log(`${data.name}님 안녕하세요`);
 }).catch((error)=>{
     console.log(error)
+}).finally(()=>{
+    console.log('마무리 작업');
 })
 
 
@@ -30,4 +32,4 @@ function App() {
 export default App;
 //promise를 사용하는 비동기 함수 만들기
 // then(), catch(), finally() Promise가 제공하는 메서드
-//then: 성공, chatch: 실패
+//then: 성공, chatch: 실패, finally: 성공과 실패 여부 상관 없이 실행
