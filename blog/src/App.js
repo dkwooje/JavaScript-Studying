ab42fcc5470ded69fbc615bb1c210e6982c4b3e5
ohwooje <dkwooje@naver.com>
2024-11-22 15:46:17 +0900

    함수 선언식과  함수 표현식

--- blog/src/App.js
+++ blog/src/App.js
@@ -1,13 +1,27 @@
 
-function main(){
-  console.log(this); 
-}
-const mainBind = main.bind({name: 'hi'}); //bind 새로운 함수를 반환해준다.
-mainBind();                           
-
 
 function App() {
 
+  main() //호이스팅 가능
+
+//함수 선언식 표현법 (일반함수) 
+  function main(){
+    console.log("hello")
+  }
+  
+  main()   //함수 main를 호출
+ 
+//함수 표현식 표현법(익명함수)
+
+  //main2() //호이스팅 불가능
+
+const main2 = function(){
+  console.log("hi")
+}
+
+  main2() //함수를 가진 변수 main2를 호출
+
 }
 export default App;
-//javascript의 this : 함수를 호출한 객체이다.
+//화살표 함수
+//function : 함수 선언식 과 함수 표현식으로 나뉜다.
