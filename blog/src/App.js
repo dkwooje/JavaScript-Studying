1a8d579c89c64f1b522d91c63757656551c59544
ohwooje <dkwooje@naver.com>
2024-11-23 17:11:58 +0900

    promise로 callback지옥을 벗어나기 후

--- blog/src/App.js
+++ blog/src/App.js
@@ -2,33 +2,59 @@
 
 function App() {
 
-  function login(username, callback){
-    setTimeout(()=>{
-      callback(username);
-    }, 1000);
+  function login(username){
+    return new Promise((resolve, reject) =>{
+      setTimeout(()=>{
+        if(username){
+          resolve(username);
+        }else{
+          reject(new Error('아이디를 입력해주세요'));
+        }
+      }, 1000); 
+    });
   }
 
-  function addToCart(product,callback){
-    setTimeout(()=>{
-      callback(product);
-    }, 1000);
+  function addToCart(product){
+    return new Promise((resolve,reject)=>{
+      setTimeout(()=>{
+        if(product){
+          resolve(product);
+        }else{
+          reject(new Error('장바구니에 넣을 상품이 없습니다.'))
+        }
+      }, 1000);
+    });
   }
   
-  function makePayment(cardNumber, product, callback){
-    setTimeout(()=>{
-      callback(cardNumber, product);
-    }, 1000);
+  function makePayment(cardNumber, product){
+    return new Promise((resolve,reject)=>{
+      setTimeout(()=>{
+        if(cardNumber.length !== 16){
+          reject(new Error('잘못된 카드 번호 입니다.'));
+          return;
+        }if(!product){
+          reject(new Error('결제할 상품을 넣어주세요'));
+          return;
+        }
+          resolve(product);
+      }, 1000);
+    });
   }
 
-  login('피코라',(username) =>{
-    console.log(`${username}님 안녕하세요.`);
-    addToCart('감자', (product) =>{
-      console.log(`${product}를 장바구니에 넣었습니다`);
-       makePayment('0000000000000000', product, (item) =>{
-        console.log(`${product}의 결제가 완료되었습니다`);
-       });
-    });
-  });
+  login('피코라').then((username) =>{
+    console.log(`${username}님 환영합니다.`);
+    return addToCart('감자');
+  })
+  .then((product) =>{
+    console.log(`${product}를 장바구니에 담았습니다.`);
+    return makePayment('1234123412341234', product);
+  })
+  .then((product) =>{
+    console.log(`${product} 결제를 완료했습니다.`);
+  })
+  .catch((error) =>{
+    console.log(error.message);
+  })
 
 }
 
