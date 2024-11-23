36810f243860b3e4624b58a1844fddbde76797c7
ohwooje <dkwooje@naver.com>
2024-11-23 20:21:35 +0900

    Await 기초 3

--- blog/src/App.js
+++ blog/src/App.js
@@ -19,14 +19,32 @@ function App() {
   //getUser가 비동기이기 때문에 networkRequest()를 기다리지 않고 바로 return을 실행한다. 
   //하지만 await를 붙이면 await가 붙여진 함수호출을 기다리고 다음을 진행할 수 있다.
   //async와 await를 사용하면 비동기 함수를 동기적으로 사용할 수 있다.
+  /*
   async function getUser(){
-   const result = await  networkRequest();
-    console.log(result);
+    await  networkRequest();
+    await  networkRequest();
      return "움직였습니다.";
   }
 
   const user = getUser();
   user.then((name)=>console.log(name))
+  */
+
+  //위의 async를 Chaining으로 만들어 보았다.
+  function getUserPromise(){
+    return networkRequest()
+    .then(()=>{
+      return networkRequest();
+    })
+    .then(()=>{
+      return "움직였습니다.";
+    });
+  }
+
+  const user = getUserPromise();
+  user.then((name)=>console.log(name))
+
+
 
 }
 
