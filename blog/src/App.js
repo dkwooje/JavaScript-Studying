56f5e28afe03afded5c91e40316a0b4844d40b8e
ohwooje <dkwooje@naver.com>
2024-11-23 19:45:46 +0900

    Async 기초 1

--- blog/src/App.js
+++ blog/src/App.js
@@ -2,46 +2,20 @@
 
 function App() {
 
-  function getName(){
-    return new Promise((resolve,reject) =>{
-      setTimeout(()=>{
-        resolve('에슈르');
-      //  reject(new Error('에러: 여기에 없어요'));
-      },3000);
-    });
+  //async는 항상 Promise를 return하는 비동기 함수이다. getUser함수는 Promise로 감싸진 상태이다.
+ async function getUser(){
+    return '다야';
   }
 
-  function getTodo(){
-    return new Promise((resolve,reject) =>{
-      setTimeout(()=>{
-      //  resolve('빵만들기');
-       reject(new Error('에러: 학교 운영하기.'));
-      },2000);
-    });
-  }
+  const user = getUser();
+  console.log(user);
 
-  getName().then((name) =>{
-    console.log(name);
-    return getTodo()
-  }).then((todo) =>{
-    console.log(todo);
-  })
+  //Promise를 반환하는 것과 같이 반환시켜주면 정상 출력이 가능하다.
+  user.then((name)=>console.log(name))
 
-//Promise.race: 전달하는 Promise 중 제일 빠른 Promise가 반환된다. any와 다르게 실패값도 마찬가지이다.
-
-const promise = Promise.race([getName(), getTodo()]);
- promise.then((data)=>{
-  console.log(data+"race")
- }).catch((error)=>{
-  console.log(error);
- })
+  
 
 }
 
 export default App;
-//promise Static 함수
-
-//1.Promise.all
-//2.Promise.allSettled
-//3.Promise.any
-//4.Promise.race
+//Asynce 와 Await
