cd4989fcfe9743f159dadc325f93b964ca10718b
ohwooje <dkwooje@naver.com>
2024-11-23 17:42:09 +0900

    promise Static 시작 전

--- blog/src/App.js
+++ blog/src/App.js
@@ -2,64 +2,39 @@
 
 function App() {
 
-  function login(username){
-    return new Promise((resolve, reject) =>{
+  function getName(){
+    return new Promise((resolve,reject) =>{
       setTimeout(()=>{
-        if(username){
-          resolve(username);
-        }else{
-          reject(new Error('아이디를 입력해주세요'));
-        }
-      }, 1000); 
+        resolve('에슈르');
+      //  reject(new Error('에러: 여기에 없어요'));
+      },1000);
     });
   }
 
-  function addToCart(product){
-    return new Promise((resolve,reject)=>{
+  function getTodo(){
+    return new Promise((resolve,reject) =>{
       setTimeout(()=>{
-        if(product){
-          resolve(product);
-        }else{
-          reject(new Error('장바구니에 넣을 상품이 없습니다.'))
-        }
-      }, 1000);
-    });
-  }
-  
-  function makePayment(cardNumber, product){
-    return new Promise((resolve,reject)=>{
-      setTimeout(()=>{
-        if(cardNumber.length !== 16){
-          reject(new Error('잘못된 카드 번호 입니다.'));
-          return;
-        }if(!product){
-          reject(new Error('결제할 상품을 넣어주세요'));
-          return;
-        }
-          resolve(product);
-      }, 1000);
+        resolve('빵만들기');
+      //  reject(new Error('에러: 학교 운영하기.'));
+      },2000);
     });
   }
 
-  login('피코라').then((username) =>{
-    console.log(`${username}님 환영합니다.`);
-    return addToCart('감자');
-  })
-  .then((product) =>{
-    console.log(`${product}를 장바구니에 담았습니다.`);
-    return makePayment('1234123412341234', product);
-  })
-  .then((product) =>{
-    console.log(`${product} 결제를 완료했습니다.`);
-  })
-  .catch((error) =>{
-    console.log(error.message);
-  })
-  .finally(()=>{
-    console.log('마무리 작업');
+  getName().then((name) =>{
+    console.log(name);
+    return getTodo()
+  }).then((todo) =>{
+    console.log(todo);
   })
 
+//getName과 getTodo를 순차적으로 실행하여 총 3초(1초+2초)의 시간이 걸렸다.
+//두 비동기 함수를 동시에(병렬) 처리하기 위해서는 Promise.all을 사용해야 한다.
 }
 
 export default App;
-//promise 응용
+//promise Static 함수
+
+//1.Promise.all
+//2.Promise.allSettled
+//3.Promise.any
+//4.Promise.race
