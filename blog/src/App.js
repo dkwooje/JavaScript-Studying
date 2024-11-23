a85aeab5d382c10f534a2b5af70b733752a8fa43
ohwooje <dkwooje@naver.com>
2024-11-23 18:42:34 +0900

    promise Static의 Promise.all 2

--- blog/src/App.js
+++ blog/src/App.js
@@ -5,8 +5,8 @@ function App() {
   function getName(){
     return new Promise((resolve,reject) =>{
       setTimeout(()=>{
-        resolve('에슈르');
-      //  reject(new Error('에러: 여기에 없어요'));
+     //   resolve('에슈르');
+        reject(new Error('에러: 여기에 없어요'));
       },1000);
     });
   }
@@ -14,8 +14,8 @@ function App() {
   function getTodo(){
     return new Promise((resolve,reject) =>{
       setTimeout(()=>{
-        resolve('빵만들기');
-      //  reject(new Error('에러: 학교 운영하기.'));
+      //  resolve('빵만들기');
+        reject(new Error('에러: 학교 운영하기.'));
       },2000);
     });
   }
@@ -28,13 +28,16 @@ function App() {
   })
 
 //getName과 getTodo를 순차적으로 실행하여 총 3초(1초+2초)의 시간이 걸렸다.
-//두 비동기 함수를 동시에(병렬) 처리하기 위해서는 Promise.all을 사용해야 한다.
+//두 비동기 함수를 동시에(병렬) 처리하기 위해서는 Promise.all을 사용해야 한
 
 const promise = Promise.all([getName(), getTodo()])
 promise.then((data)=>{
   console.log(data);
-});
-
+}).catch((error)=>{
+  console.log(error);
+})
+// Promise.all안의 배열에서 하나라도 실패하면 reject를 반환시키고 catch블록이 실행된다.
+// catch는 가장 먼저 실패한 promise가 실행된다.
 
 }
 
