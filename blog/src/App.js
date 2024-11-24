a940546dc38990a15450570e167e71d062b6aeb4
ohwooje <dkwooje@naver.com>
2024-11-24 21:20:38 +0900

    console.log 사용법  console.table

--- blog/src/App.js
+++ blog/src/App.js
@@ -2,9 +2,21 @@
 
 function App() {
 
-console.log("호출");
-console.error("에러");
-console.warn("경고");
+console.log([1,2,3]);
+console.table([1,2,3]);
+console.table([
+  {
+  x:10,
+  y:20,
+  },
+  {
+  x:11,
+  y:21,
+  },
+  {
+  x:12,
+  y:22,
+  }]);
 
 }
 export default App;
@@ -12,4 +24,5 @@ export default App;
 //console.log의 다양한 기능.
 //1. %c : css 스타일 적용.
 //2. console.group :콘솔을 그룹으로 묶어서 표현.
-//3. console.Error, console.warn
+//3. console.Error, console.warn :콘솔에 경고 UI 추가
+//4. console.table: 콘솔로 표현하는 배열을 테이블화
