a8d0e928c483f560c9dc342b12f6448ec930d5a7
ohwooje <dkwooje@naver.com>
2024-11-24 21:07:03 +0900

    console.log 사용법  console.group

--- blog/src/App.js
+++ blog/src/App.js
@@ -2,12 +2,36 @@
 
 function App() {
 
+console.group("루프 1");
+for (let i = 0; i<3; i++){
+  console.log(i);
+}
+console.groupEnd();
+console.group("루프 2");
+for (let i = 0; i<3; i++){
+  console.log(i);
+}
+console.groupEnd();
+console.group("루프 3");
+for (let i = 0; i<3; i++){
+  console.log(i);
+}
+console.groupEnd();
+console.group("루프 4");
+for (let i = 0; i<3; i++){
+  console.log(i);
+}
+console.groupEnd();
+console.group("루프 5");
+for (let i = 0; i<3; i++){
+  console.log(i);
+}
+console.groupEnd();
 
-console.log("%c엘프제국에 영광을",
-   'color:yellow; font-weight: bold; background-color: black; padding: 5px')
 
 }
 export default App;
 
 //console.log의 다양한 기능.
 //1. %c : css 스타일 적용.
+//2. console.group :콘솔을 그룹으로 묶어서 표현.
