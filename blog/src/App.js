c1257d61d11b9e4d3e9a9a2d13a1c0c2d8fa7eb9
ohwooje <dkwooje@naver.com>
2024-11-24 21:03:35 +0900

    console.log 사용법  %c

--- blog/src/App.js
+++ blog/src/App.js
@@ -2,24 +2,12 @@
 
 function App() {
 
-const users = 
-  {name: '피라', age: 540}
 
-
-  function printUser1(user){
-    console.log(`${user.name}님은 ${user.age}살 입니다.`)
-  }
-  
-//혹은
-
-  function printUser2({name,age}){
-    console.log(`${name}님은 ${age}살 입니다.`)
-  }
-
-  printUser1(users);
-  printUser2(users);
+console.log("%c엘프제국에 영광을",
+   'color:yellow; font-weight: bold; background-color: black; padding: 5px')
 
 }
 export default App;
-//구조 분해 할당: 배열이나 객체를 분해해서 각각의 값을 개별적인 변수에 담는 표현식.
 
+//console.log의 다양한 기능.
+//1. %c : css 스타일 적용.
