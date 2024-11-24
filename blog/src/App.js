df0a2e425701f056287a8dc881eb4940a648da7d
ohwooje <dkwooje@naver.com>
2024-11-24 20:05:40 +0900

    구조 분해 할당. 배열 구조 분해 할당 1

--- blog/src/App.js
+++ blog/src/App.js
@@ -2,28 +2,23 @@
 
 function App() {
 
-  const cart = [
-    {
-     name: '사과',
-     price: 500
-    },
-    {
-     name: '바나나',
-     price: 700
-    },
-    {
-     name: '레몬',
-     price: 300
-    },
-  ]
-  
- const allPrice = cart.reduce((acc,cur)=>{
-  return acc + cur.price
- },0)
+  //배열 구조 분해 할당.
+  const animals = ['강아지','고양이','오리'];
 
- console.log(allPrice);
+  const dog = animals[0];
+  const cat = animals[1];
+  console.log(dog);
+  console.log(cat);
+
+  const [dog1, cat1] = animals;
+  console.log(dog1, cat1);
+
+  const [dog2, ,duck2] = animals;
+  console.log(dog2, duck2);
+
+  const [dog3, cat3, duck3="꽥꽥", cow3="소"] =animals;
+  console.log(duck3, cow3);
 
 }
 export default App;
-//reduce : 배열 안의 모든 요소를 하나의 값으로 만들때 사용
-//reduce함수는 배열의 각 요소가 한번씩 실행한다.
+//구조 분해 할당: 배열이나 객체를 분해해서 각각의 값을 개별적인 변수에 담는 표현식.
