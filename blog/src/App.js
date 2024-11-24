980955919c72a4dec3b481c00bc807704904095a
ohwooje <dkwooje@naver.com>
2024-11-24 19:27:51 +0900

    배열 함수 reduce

--- blog/src/App.js
+++ blog/src/App.js
@@ -2,27 +2,32 @@
 
 function App() {
 
-  const fruits = [
-    { name: '배', price: 500},
-    { name: '사과', price: 700},
-    { name: '귤', price: 400},
-    { name: '포도', price: 1500},
-  ]
-//키와값 형태의 객체 배열에서 단순 문자열 배열로 바꿈
-  const priceTags = fruits.map((fruit)=>{   
-    return `${fruit.name} : ${fruit.price}원`
-  });
+const numbers = [1,2,3,4];
 
-//키와 값 형태로 유지할 수 있다.
-  const priceValue = fruits.map((fruit) => { 
-    return { name: `맛있는 ${fruit.name}`, priceTag: `${fruit.price}원` };
-  });
+//forEach
+let total1 = 0;
 
-  console.log(priceTags);
-  console.log(fruits);
-  console.log(priceValue);
+numbers.forEach(number =>{
+  total1 = total1 + number;
+})
+
+console.log(total1);
+
+//reduce
+//accumulator: 현재까지 누적된 값을 의미
+//currentValue: 각 배열의 요소를 의미
+const total2 = numbers.reduce((accumulator,currentValue)=>{
+   return accumulator + currentValue
+}, 0) //accumulator의 초기값: 0
+//accumulator: 0  currentValue: 1  return:  1
+//accumulator: 1  currentValue: 2  return:  3
+//accumulator: 3  currentValue: 3  return:  6
+//accumulator: 6  currentValue: 4  return:  10
+
+console.log(total2);
+
+
 }
 export default App;
-
-// map(): 원본 배열의 요소를 다른 형태로 변환해에 담아서 새로운 배열로 만든다.
-//원본배열은 바뀌지 않는다.
+//reduce : 배열 안의 모든 요소를 하나의 값으로 만들때 사용
+//reduce함수는 배열의 각 요소가 한번씩 실행한다.
