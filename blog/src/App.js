8f44169c802cc5e57d35d42afec46de1c372624c
ohwooje <dkwooje@naver.com>
2024-11-24 20:47:54 +0900

    구조 분해 할당. 응용 2

--- blog/src/App.js
+++ blog/src/App.js
@@ -2,15 +2,24 @@
 
 function App() {
 
-const users = [
-  {name: '피라', age: 540},
-  {name: '키디언', age: 485}
-]
+const users = 
+  {name: '피라', age: 540}
 
-  for(const user of users){
-    console.log(user.name);
+
+  function printUser1(user){
+    console.log(`${user.name}님은 ${user.age}살 입니다.`)
   }
+  
+//혹은
+
+  function printUser2({name,age}){
+    console.log(`${name}님은 ${age}살 입니다.`)
+  }
+
+  printUser1(users);
+  printUser2(users);
 
 }
 export default App;
 //구조 분해 할당: 배열이나 객체를 분해해서 각각의 값을 개별적인 변수에 담는 표현식.
+
