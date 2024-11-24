f077300089c3f634a391cf6539e61c290e72fb0e
ohwooje <dkwooje@naver.com>
2024-11-24 21:25:12 +0900

    console.log 사용법  console.time

--- blog/src/App.js
+++ blog/src/App.js
@@ -2,22 +2,12 @@
 
 function App() {
 
-console.log([1,2,3]);
-console.table([1,2,3]);
-console.table([
-  {
-  x:10,
-  y:20,
-  },
-  {
-  x:11,
-  y:21,
-  },
-  {
-  x:12,
-  y:22,
-  }]);
-
+console.time("타이머1");
+console.time("타이머2");
+for (let i=0; i<5000000000; i++){}
+console.timeEnd("타이머1");
+for (let i=0; i<5000000000; i++){}
+console.timeEnd("타이머2");
 }
 export default App;
 
@@ -26,3 +16,4 @@ export default App;
 //2. console.group :콘솔을 그룹으로 묶어서 표현.
 //3. console.Error, console.warn :콘솔에 경고 UI 추가
 //4. console.table: 콘솔로 표현하는 배열을 테이블화
+//5. console.time : 시간 측정
