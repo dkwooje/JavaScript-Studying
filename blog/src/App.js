b2d0441432c189b110688e7209ed98473863bd13
ohwooje <dkwooje@naver.com>
2024-11-24 20:28:28 +0900

    구조 분해 할당. 객체 구조 분해 할당 2

--- blog/src/App.js
+++ blog/src/App.js
@@ -9,20 +9,19 @@ const animals ={
   duck: '오리'
 }
 
-const dog1 = animals.dog;
-const cat1 = animals.cat;
-const duck1 = animals.duck;
-
-console.log(dog1,cat1,duck1);
-
 
  //객체 구조 분해 할당.
  //기존 객체와 같은 이름을 가져야 한다.
  //위치를 바꿔도 생략도 수정도 가능하다
- const {dog,cat,duck} = animals
- console.log(dog,cat,duck);
- console.log(duck,dog);
 
+ const dog = '멍멍';
+ const cat = '야옹';
+ const duck = '꽥꽥';
+ const {dog: dogName, cat: catName, duck: duckName} = animals
+ console.log(dog,cat,duck);
+ console.log(dogName,catName,duckName);
+ 
+ 
 }
 export default App;
 //구조 분해 할당: 배열이나 객체를 분해해서 각각의 값을 개별적인 변수에 담는 표현식.
